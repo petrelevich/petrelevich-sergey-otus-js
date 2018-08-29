71e9284424cf4ac262175f3197fa07d71acae0e8
petrelevich <petrelevich@yandex.ru>
2018-08-29 23:19:03 +0300

    HW 03 fix code-review comments

--- javascript/HW03/tree.js
+++ javascript/HW03/tree.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+var dirFunctions = require('./dirFunctions');
 
 if (process.argv.length <= 2) {
     console.log("Usage: " + __filename + " path/to/directory");
@@ -6,38 +6,5 @@ if (process.argv.length <= 2) {
 }
 
 const rootPath = process.argv[2];
-const PATH_SEPARATOR = require('path').sep;
 
-const getFullName = (path, item) => {
-    return `${path}${PATH_SEPARATOR}${item}`;
-};
-
-const isDir = (path, item) => {
-    return fs.lstatSync(getFullName(path, item)).isDirectory();
-};
-
-const results = {files:[], dirs: []};
-
-const dirList = (path) => {
-    return new Promise((resolve) => {
-        fs.readdir(path, function(err, items) {
-            let innerPromise = undefined;
-            items.forEach(item => {
-                if (isDir(path, item)) {
-                    results.dirs.push(getFullName(path, item));
-                    innerPromise = dirList(getFullName(path, item));
-                } else {
-                    results.files.push(getFullName(path, item));
-                }
-            });
-
-            if (innerPromise !== undefined) {
-                innerPromise.then(() => { resolve(results) });
-            } else {
-                resolve();
-            }
-        });
-    })
-};
-
-dirList(rootPath).then(console.log);
+dirFunctions.dirList(rootPath, (err, results) => {console.log(results)});
